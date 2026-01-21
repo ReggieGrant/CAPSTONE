a30ed06fd79bd0a2a6fee22c445e2d78410a922c
ReggieGrant <grantreggie09@gmail.com>
2026-01-20 19:49:46 -0800

    location page/login functionality

--- users/urls.py
+++ users/urls.py
@@ -5,8 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    # Define user-related URL patterns here
-   path('login/', views.UserLoginView.as_view(), name='login'),
-   path('logout/', views.UserLogoutView.as_view(), name='logout'),
-   path('register/', views.UserRegisterView.as_view(), name='register'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('register/', views.UserRegisterView.as_view(), name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,15 +1,34 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+
 
 class UserLoginView(LoginView):
     template_name = 'users/login.html'
 
+    def get_success_url(self):
+        return reverse_lazy('profile')
+
 class UserLogoutView(LogoutView):
     next_page = reverse_lazy('login')
 
-class UserRegisterView(CreateView):
+
+
+class UserRegisterView(CreateView, forms.ModelForm):
     form_class = UserCreationForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('login')
+    model = User
+
+    def get_form_class(self):
+        return UserCreationForm
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        pass_text = form.cleaned_data['password1']
+        user.save()
+
+        return super().form_valid(form)
